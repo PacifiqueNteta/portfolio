f2b4778d5a6ce06fbd852ee279dc6ed3ac25fbea
Pacifique Nteta <pnteta311@gmail.com>
2026-02-18 22:24:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["An Industrial Engineer", "A Data Analyst", "A Data Engineer"],
+        strings: ["A Data Engineer","A Data Analyst", "An Industrial Engineer"],
         typeSpeed: 90,
         backSpeed: 60,
         loop: true
